dff59042d4985d5c5b8e3b0a2066613af593f203
elvishuges <elvishuges@hotmail.com>
2019-01-17 01:14:59 -0300

    add componentes da pagina

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -1,16 +1,13 @@
 import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
 import React from 'react'
-import Todo from '../todo/todo'
-import About from '../about/about'
 import Menu from '../templete/menu'
+import Routes from './routes'
 
 export default props =>(
 
-    <div className="container">
-      <h1>Teste</h1>
-      <Menu/>
-      <Todo/>
-      <About/>      
+    <div className="container">      
+      <Menu/>      
+      <Routes/>   
     </div>
 )
--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -1,12 +1,16 @@
-import React,{Component} from 'react'
+import React, { Component } from "react";
+import PageHeader from "../template/pageHeader";
+import TodoForm from "./todoForm";
+import TodoList from "./todoList";
 
-export default class Todo extends Component{
-
-    render(){
-        return(
-            <div>
-                <h1>Todo </h1>
-            </div>
-        )
-    }
+export default class Todo extends Component {
+  render() {
+    return (
+      <div>
+        <PageHeader name="Tarefas" small="Cadastro" > </PageHeader>
+        <TodoForm/>
+        <TodoList/>
+      </div>
+    )
+  }
 }
