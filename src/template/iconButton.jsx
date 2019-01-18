6118751729bbd4c85b73416334d1654a84a937ef
elvishuges <elvishuges@hotmail.com>
2019-01-18 00:24:36 -0300

    componentes grid e iconButton

--- src/template/iconButton.jsx
+++ src/template/iconButton.jsx
@@ -7,7 +7,9 @@ export default props =>{
     }
     else {
         return(
-            <button className={'btn btn-'+ props.style }>
+            <button className={'btn btn-'+ props.style }
+             onClick={props.onClick}
+            >
                 <i className={"fa fa-"+ props.icon} />
             </button>
         )
--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import IconButton from '../template/iconButton'
+import Grid from '../template/grid'
 
 export default props => (
-    <div role="form" className="todoFor,">
-        <div className="col-xs-12 col-sm-9 col-md-10">
+    <div role="form" className="todoForm">
+       <Grid cols='12 9 10'>
             <input
                 id="description"
                 className="form-control"
                 placeholder="Adicione uma tarefa"
             />
-        </div>
-
-        <div className="col-xs-12 col-sm-3 col-md-2">
+        </Grid>
+        <Grid cols='12 3 2'>        
            <IconButton style="primary" icon="plus" > </IconButton>
-        </div>
+        </Grid>
     </div>
 )
