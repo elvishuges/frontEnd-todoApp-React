3cd74ec6fa281f875e640358df4da04b77c0d03a
elvishuges <elvishuges@hotmail.com>
2019-01-23 00:24:39 -0300

    adicionando teclas de atalho

--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -2,31 +2,44 @@ import React from "react";
 import IconButton from '../template/iconButton'
 import Grid from '../template/grid'
 
-export default props => (
+export default props => {
+     
+    const keyHandler = (e) => {
+        if(e.key === 'Enter'){
+            e.shiftKey ? props.handleSearch() : props.handleAdd()
+        }
+        else if (e.key === 'Escape') {
+            props.handleClear()
+        }
+    }
+    
+    
+    return (
     <div role="form" className="todoForm">
-       <Grid cols='12 9 10'>
-            <input
-                type="text"
-                id="description"
-                className="form-control"
-                placeholder="Adicione uma tarefa"
-                value = {props.description}
-                onChange = {props.handleChange}
-            />
-            
-        </Grid>
-        <Grid cols='12 3 2'>        
-           <IconButton onClick={props.handleAdd} 
-           style="primary" 
-           icon="plus" > </IconButton>
-           <IconButton 
-           onClick={props.handleSearch} 
-           style="info" 
-           icon="search" > </IconButton>
-           <IconButton
-            style='default' icon='close'
-            onClick = {props.handleClear}
-           ></IconButton>
-        </Grid>
-    </div>
-)
+    <Grid cols='12 9 10'>
+         <input
+             type="text"
+             id="description"
+             className="form-control"
+             placeholder="Adicione uma tarefa"
+             onKeyUp = {keyHandler}
+             value = {props.description}
+             onChange = {props.handleChange}
+         />
+         
+     </Grid>
+     <Grid cols='12 3 2'>        
+        <IconButton onClick={props.handleAdd} 
+        style="primary" 
+        icon="plus" > </IconButton>
+        <IconButton 
+        onClick={props.handleSearch} 
+        style="info" 
+        icon="search" > </IconButton>
+        <IconButton
+         style='default' icon='close'
+         onClick = {props.handleClear}
+        ></IconButton>
+     </Grid>
+ </div>
+)}
